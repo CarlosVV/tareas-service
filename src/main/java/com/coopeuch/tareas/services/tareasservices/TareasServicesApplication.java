f69a58f58048dcc0446420ec6a6cb5cedf744344
Carlos Valderrama <cvalderramav@gmail.com>
2020-10-14 20:16:24 -0300

    first commit

---
+++ src/main/java/com/coopeuch/tareas/services/tareasservices/TareasServicesApplication.java
@@ -0,0 +1,13 @@
+package com.coopeuch.tareas.services.tareasservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareasServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coopeuch/tareas/services/tareasservices/TareasServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coopeuch.tareas.services.tareasservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
